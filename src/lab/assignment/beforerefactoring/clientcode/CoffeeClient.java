8928ffbf56ac4afa6b3d70acf2e997b2357b8404
lindlands <lindlands@outlook.com>
2024-05-06 13:03:56 -0700

    Coffee client added

--- src/lab/assignment/beforerefactoring/clientcode/CoffeeClient.java
+++ src/lab/assignment/beforerefactoring/clientcode/CoffeeClient.java
@@ -1,17 +1,26 @@
 package lab.assignment.beforerefactoring.clientcode;
 
 import lab.assignment.beforerefactoring.CoffeeMaker;
+import lab.assignment.beforerefactoring.MochaMaker;
+import lab.assignment.beforerefactoring.CapuccinoMaker;
+import lab.assignment.beforerefactoring.AmericanoMaker;
 
 public class CoffeeClient {
 
 
     public static void main(String[] args) {
-        CoffeeMaker starbuzz = new CoffeeMaker();
-        starbuzz.prepareMochaCoffee();
+        CoffeeMaker starbuzz = new MochaMaker();
+        starbuzz.prepareCoffee();
 
         System.out.println("********************");
 
-        starbuzz.prepareCapuccinoCoffee();
+        starbuzz = new CapuccinoMaker();
+        starbuzz.prepareCoffee();
+
+        System.out.println("********************");
+
+        starbuzz = new AmericanoMaker();
+        starbuzz.prepareCoffee();
 
     }
 
